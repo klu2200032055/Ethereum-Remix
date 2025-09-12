0a4017056210e8ec15482c9553da4b3c2d6d38e0
A LIKHIL NAGA CHAITANYA <2200032055@kluniversity.in>
2025-09-12 07:39:59 +0530

    Require.sol

---
+++ Require.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract RequireStatement {
+    function checkInput(uint _input) public pure returns (string memory) {
+        require(_input <= 255, "invalid uint8");
+        return "Input is Uint8";
+    }
+
+    function odd(uint _input) public pure returns (bool) {
+        require(_input % 2 != 0, "Not an odd number");
+        return true;
+    }
+}
